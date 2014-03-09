0c291b43e29165d10008293a49787f2a50ec9bc4
deionalims <fx.oxeda@gmail.com>
2014-03-09 23:18:42 +0100

    Creating MediaPlayerService

--- prysmradio/src/main/java/com/prysmradio/services/MediaPlayerService.java
+++ prysmradio/src/main/java/com/prysmradio/services/MediaPlayerService.java
@@ -1,6 +1,7 @@
 package com.prysmradio.services;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -8,37 +9,61 @@ import android.net.Uri;
 import android.os.IBinder;
 
 import com.prysmradio.R;
+import com.prysmradio.utils.Constants;
 
 import java.io.IOException;
 
-/**
- * Created by fxoxe_000 on 09/03/14.
- */
-public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener {
 
-    MediaPlayer mMediaPlayer = null;
+public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, AudioManager.OnAudioFocusChangeListener {
+
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
     public void onCreate() {
-        super.onCreate();
+        mMediaPlayer = new MediaPlayer();
+
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnPreparedListener(this);
+        mMediaPlayer.setOnErrorListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        mMediaPlayer.release();
+        mMediaPlayer = null;
     }
 
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        String action = intent.getAction();
 
-
-        try {
-            mMediaPlayer.setDataSource(this, Uri.parse(getString(R.string.radio_url))); // Go to Initialized state
-            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-            mMediaPlayer.setOnPreparedListener(this);
-            mMediaPlayer.prepareAsync(); // prepare async to not block main thread
-        } catch (IOException e){
-
+        if (action.equals(Constants.START_SERVICE_ACTION)) {
+            start();
+        } else if (action.equals(Constants.STOP_SERVICE_ACTION)){
+            stop();
         }
 
-
         return START_NOT_STICKY;
+    }
+
+    public synchronized void start(){
+        try {
+            mMediaPlayer.setDataSource(this, Uri.parse(getString(R.string.radio_url))); // Go to Initialized state
+            mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+        } catch (IOException e){
+            mMediaPlayer.reset();
+        }
+    }
+
+    public synchronized void stop(){
+        if (mMediaPlayer.isPlaying()){
+            mMediaPlayer.stop();
+        }
+
+        mMediaPlayer.reset();
+        // TODO Stop foreground
+
+        stopSelf();
     }
 
 
@@ -49,6 +74,25 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
 
     @Override
     public void onPrepared(MediaPlayer mp) {
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int result = audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC,
+                AudioManager.AUDIOFOCUS_GAIN);
+
+        if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mMediaPlayer.reset();
+        } else {
+            mMediaPlayer.start();
+            //TODO Start fore ground
+        }
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
 
     }
 }
---
+++ prysmradio/src/main/java/com/prysmradio/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.prysmradio.utils;
+
+
+public class Constants {
+
+    // Media Service Actions
+    public static final String START_SERVICE_ACTION = "com.prysmradio.service_start";
+    public static final String STOP_SERVICE_ACTION = "com.prysmradio.service_stop";
+
+}
